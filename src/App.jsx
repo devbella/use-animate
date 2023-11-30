7400d5db3574fd9de077801d970c575a7bd7227f
devbella <bellachoi1210@gmail.com>
2023-11-30 10:00:31 +0900

    Use-Animate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <img
+        className="w-96 h-96
+        animate__animated animate__heartBeat"
+        src="./images/unicorn.png"
+        alt="unicorn"
+      />
+    </div>
+  );
 };
 
 export default App;
